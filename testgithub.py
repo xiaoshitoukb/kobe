682a594dfdebfadfcaefed81bd9065d874c97452
yanglei <yanglei@dongyin.net.cn>
2019-07-17 21:17:45 +0800

    写了一个素数的小函数

---
+++ testgithub.py
@@ -0,0 +1,15 @@
+
+#print sushu
+#author kobe.yang
+
+def sushu(n):
+    for i in range(2, n):
+        for x in range(2, i):
+            if i % x == 0:
+                #print(i, 'not zhishu')
+                break
+        else:
+            print(i, 'zhishu')
+
+
+sushu(100)
